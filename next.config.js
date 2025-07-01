3da3d997674b66e9b4eb1c92a67774c597808907
Gabriel Freitas <69334324+gabrielfreitasc@users.noreply.github.com>
2025-06-30 22:21:21 -0300

    fix error build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
-const path = require('path');
+const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
+  distDir: process.env.NEXT_DIST_DIR || ".next",
+  // Remove the output configuration that's causing the build issue
+  // output: process.env.NEXT_OUTPUT_MODE,
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
+    outputFileTracingRoot: path.join(__dirname, "../"),
   },
   eslint: {
     ignoreDuringBuilds: true,
